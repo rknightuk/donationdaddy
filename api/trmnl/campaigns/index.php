6016b34753ceb5ac6fb2fdfc49ef46da46582ebe
Robb Knight <desk@rknight.me>
2025-06-09 10:32:13 +0100

    this

--- api/trmnl/campaigns/index.php
+++ api/trmnl/campaigns/index.php
@@ -5,6 +5,7 @@ require '../../../vendor/autoload.php';
 use DonationDaddy\Data;
 
 header('Access-Control-Allow-Headers: *');
+header('Access-Control-Allow-Origin: *');
 header("Content-Type: application/json");
 
 $data = array_map(function($campaign) {
